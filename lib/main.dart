a5f1c35e159810a42d969a5f5d962b9ed2670366
DoisLucas <pedrolucas499@gmail.com>
2019-06-07 21:38:20 -0300

    Handle Text controller and logic

--- lib/main.dart
+++ lib/main.dart
@@ -12,69 +12,109 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+
+  String info = "Informe seus dados!";
+
+  void _calc() {
+    double peso = double.parse(pesoController.text);
+    double altura = double.parse(alturaController.text) / 100;
+    double imc = peso / (altura * altura);
+
+    print(imc);
+
+    String imc_formated = imc.toStringAsPrecision(2);
+
+    setState(() {
+      if (imc < 18.6) {
+        info = "Abaixo do peso ($imc_formated)";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        info = "Peso ideial ($imc_formated)";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        info = "Levemente acima do peso ($imc_formated)";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        info = "Obesidade Grau I ($imc_formated)";
+      } else if (imc >= 34.9 && imc < 34.9) {
+        info = "Obesidade Grau II ($imc_formated)";
+      } else if (imc >= 40) {
+        info = "Obesidade Grau III ($imc_formated)";
+      }
+    });
+  }
+
+  void _resetFields() {
+    pesoController.text = "";
+    alturaController.text = "";
+
+    setState(() {
+      info = 'Informe seus dados!';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black45,
-        title: Text("Calculadora de IMC"),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body:
-
-      SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(Icons.person_outline, size: 110, color: Colors.black54),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Peso (kg)",
-                    labelStyle: TextStyle(color: Colors.black45)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black45, fontSize: 20),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10, bottom: 10),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Altura (cm)",
-                    labelStyle: TextStyle(color: Colors.black45)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black45, fontSize: 20),
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.all(10),
-                child: Container(
-                  height: 45,
-                  child: RaisedButton(
-                    onPressed: () {},
-                    child: Text("Calcular",
-                        style: TextStyle(color: Colors.white, fontSize: 15)),
-                    color: Colors.black45,
-                  ),
-                )),
-            Text("Resultado do IMC",
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black45, fontSize: 20, fontWeight: FontWeight.bold))
+        appBar: AppBar(
+          backgroundColor: Colors.black45,
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _resetFields,
+            )
           ],
         ),
-      )
-
-
-    );
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 110, color: Colors.black54),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Peso (kg)",
+                      labelStyle: TextStyle(color: Colors.black45)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black45, fontSize: 20),
+                  controller: pesoController,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10, right: 10),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Altura (cm)",
+                      labelStyle: TextStyle(color: Colors.black45)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black45, fontSize: 20),
+                  controller: alturaController,
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Container(
+                    height: 45,
+                    child: RaisedButton(
+                      onPressed: _calc,
+                      child: Text("Calcular",
+                          style: TextStyle(color: Colors.white, fontSize: 15)),
+                      color: Colors.black45,
+                    ),
+                  )),
+              Text(info,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black45,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold))
+            ],
+          ),
+        ));
   }
 }
