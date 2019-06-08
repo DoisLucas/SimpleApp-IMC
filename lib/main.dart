b229e4108f4ca16ee3b7ac1724f5ce21d074fe57
DoisLucas <pedrolucas499@gmail.com>
2019-06-07 21:05:52 -0300

    Body layout, TextField, ScrollView, Container and Icon

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,59 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-          onPressed: () {},
-      )
+            onPressed: () {},
+          )
         ],
       ),
+      backgroundColor: Colors.white,
+      body:
+
+      SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_outline, size: 110, color: Colors.black54),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.black45)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black45, fontSize: 20),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.black45)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black45, fontSize: 20),
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  height: 45,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text("Calcular",
+                        style: TextStyle(color: Colors.white, fontSize: 15)),
+                    color: Colors.black45,
+                  ),
+                )),
+            Text("Resultado do IMC",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black45, fontSize: 20, fontWeight: FontWeight.bold))
+          ],
+        ),
+      )
+
+
     );
   }
 }
